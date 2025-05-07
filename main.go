60a5dbb9e969124f82ece6218d2060931882a32e
KoZin <zinminthu.dev@gmail.com>
2025-05-07 22:58:10 +0700

    add a constructor function and method

--- main.go
+++ main.go
@@ -1,21 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"github.com/zin-min-thu/go-price-calculator/prices"
+)
 
 func main() {
-	prices := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
-	result := make(map[float64][]float64)
+	// result := make(map[float64][]float64)
 
 	for _, taxRate := range taxRates {
-		taxIncludedPrices := make([]float64, len(prices))
-		for priceIndex, price := range prices {
-			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
-		}
-
-		result[taxRate] = taxIncludedPrices
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 	}
 
-	fmt.Println(result)
+	// fmt.Println(result)
 }
