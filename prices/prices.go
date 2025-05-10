5e3ab282023e0c2eeaa4112a5d852eb72520dc03
KoZin <zinminthu.dev@gmail.com>
2025-05-10 20:57:27 +0700

    fix bugs

--- prices/prices.go
+++ prices/prices.go
@@ -8,10 +8,10 @@ import (
 )
 
 type TaxIncludedPriceJob struct {
-	IOManager         filemanager.FileManager `json:"-"`
+	IOManager         filemanager.FileManager `json:"-"` // not include in json
 	TaxRate           float64                 `json:"tax_rate"`
 	InputPrices       []float64               `json:"input_prices"`
-	TaxIncludedPrices map[string]string       `json:tax_included_prices"`
+	TaxIncludedPrices map[string]string       `json:"tax_included_prices"`
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
